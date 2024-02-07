1be24115618cdc31bf3752b5cef20f86efed8779
Jacques Navarro <4421229+bunny-thief@users.noreply.github.com>
2024-02-07 11:16:45 +0100

    Initial commit

---
+++ src/main/java/dev/passingarguments/countriesapi/CountriesApiApplication.java
@@ -0,0 +1,13 @@
+package dev.passingarguments.countriesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesApiApplication.class, args);
+	}
+
+}
